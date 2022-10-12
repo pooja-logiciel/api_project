512b0e77e2b14c578c36d71fb09675320f4d1f90
pooja-logiciel <112501459+pooja-logiciel@users.noreply.github.com>
2022-10-12 11:11:05 +0530

    Add files via upload

---
+++ food_recipes/navbar.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+  	<nav class="navbar navbar-expand-lg navbar-light bg-light shadow p-3 mb-5 bg-body rounded">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="#">Navbar</a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+				<form class="d-flex" method ="get">
+					<input class="form-control me-2 " type="search" name="text" placeholder="Search" aria-label="Search">
+					<input type="submit" class="btn btn-outline-success" name="submit">
+				</form>
+			</div>
+		</div>
+	</nav>
+</body>
+</html>
